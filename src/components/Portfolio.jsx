9617936593f28c3bdf0793ed61899179d4e9e081
Vasilya Nizamova <victoriartz89@gmail.com>
2020-07-15 10:36:33 -0400

    slideshow functionality finish

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,82 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ProjectCard from "./ProjectCard";
-import bG1 from "../img/project-background.jpg";
-import bG2 from "../img/project-background2.jpg";
-import bG3 from "../img/project-background3.jpg";
-import bG4 from "../img/project-background4.jpg";
+import React, { Component } from "react";
+import Projects from "../Projects.json";
+import Project from "./Project";
 
-export default function Portfolio() {
 
-  return (
+class Portfolio extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: Projects,
+      project: Projects[0]
+    }
+  } 
+  nextProject = () => {
+    const newIndex = this.state.project.index + 1;
+    this.setState({
+      project: Projects[newIndex]
+    })
+  }
+  prevProject = () => {
+    const newIndex = this.state.project.index - 1;
+    this.setState({
+      project: Projects[newIndex]
+    })
+  }
+
+  render() {
+    const { projects, project } = this.state;
+    return (
       <div className="portfolio">
-        <div className="grid-container">
-          <Link id="project1" to="/project1" className="area">
-            <div
-              className="image"
-              style={{
-                backgroundImage: "url(" + bG1 + ")",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                width: "auto",
-                minHeight: "100%",
-              }}
-            >
-              <div className="overlay">
-                <h4>Project I</h4>
-              </div>
-            </div>
-          </Link>
-          <Link id="project2" to="/project2" className="area">
-            <div
-              className="image"
-              style={{
-                backgroundImage: "url(" + bG2 + ")",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                width: "auto",
-                minHeight: "100%",
-              }}
-            >
-              <div className="overlay">
-                <h4>Project II</h4>
-              </div>
-            </div>
-          </Link>
-          <Link id="project3" to="/project3" className="area">
-            <div
-              className="image"
-              style={{
-                backgroundImage: "url(" + bG3 + ")",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                width: "auto",
-                minHeight: "100%",
-              }}
-            >
-              <div className="overlay">
-                <h4>Project III</h4>
-              </div>
-            </div>
-          </Link>
-          <Link id="project4" to="/project4" className="area">
-            <div
-              className="image"
-              style={{
-                backgroundImage: "url(" + bG4 + ")",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                width: "auto",
-                minHeight: "100%",
-              }}
-            >
-              <div className="overlay">
-                <h4>Project IV</h4>
-              </div>
-            </div>
-          </Link>
-        </div>
-        <ProjectCard />
+        <button
+          onClick={() => this.nextProject()}
+          disabled={project.index === Projects.length - 1}
+        >
+          Next
+        </button>
+        <button
+          onClick={() => this.prevProject()}
+          disabled={project.index === 0}
+        >
+          Prev
+        </button>
+        <Project project={project} />
       </div>
-    );
-};
+    )
+  }
+}
+export default Portfolio;
